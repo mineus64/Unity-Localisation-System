5db5b8356ea7c349780e3b7511e6ee6aefd5cb09
mineus64 <43474851+mineus64@users.noreply.github.com>
2022-01-31 23:06:26 -0800

    Updated to Latest
    - Updated to latest version of the localisation system, used in my own projects
    - Created Editor folder to hold editor scripts
    - Added LocalisedStringDrawer
    - Added TextLocaliserEditor
    - Created Resources folder
    - Moved images to the Resources folder
    - Trimmed down the localisation CSV to just the essentials

---
+++ Editor/TextLocaliserEditor.cs
@@ -0,0 +1,111 @@
+﻿// Component of the Unity Localisation System developed by Nathan Hodson (Subgiant Games) based off of the excellent videos by Matt of GameDevGuide on YouTube
+// Use it how you like but please make sure to credit me if you do!
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class TextLocaliserEditWindow : EditorWindow 
+{
+    public static void Open(string key) 
+    {
+      var window = (TextLocaliserEditWindow)ScriptableObject.CreateInstance(typeof(TextLocaliserEditWindow));
+      window.titleContent = new GUIContent("Localiser Window");
+      window.ShowUtility();
+      window.key = key;
+    }
+
+    public string key;
+    public string value;
+
+    public void OnGUI() 
+    {
+      key = EditorGUILayout.TextField("Key:", key);
+      EditorGUILayout.BeginHorizontal();
+      EditorGUILayout.LabelField("Value:", GUILayout.MaxWidth(50));
+
+      EditorStyles.textArea.wordWrap = true;
+      value = EditorGUILayout.TextArea(value, EditorStyles.textArea, GUILayout.Height(100), GUILayout.Width(400));
+      EditorGUILayout.EndHorizontal();
+
+      if (GUILayout.Button("Add")) {
+        if (LocalisationSystem.GetLocalisedValue(key) != string.Empty) {
+          LocalisationSystem.Replace(key, value);
+        }
+        else {
+          LocalisationSystem.Add(key, value);
+        }
+      }
+
+      minSize = new Vector2(460, 250);
+      maxSize = minSize;
+    }
+}
+
+public class TextLocaliserSearchWindow : EditorWindow 
+{
+    public static void Open() 
+    {
+      var window = (TextLocaliserSearchWindow)ScriptableObject.CreateInstance(typeof(TextLocaliserSearchWindow));
+      window.titleContent = new GUIContent("Localisation Search");
+
+      Vector2 mouse = GUIUtility.GUIToScreenPoint(Event.current.mousePosition);
+      Rect r = new Rect(mouse.x - 450, mouse.y + 10, 10, 10);
+      window.ShowAsDropDown(r, new Vector2(500, 300));
+    }
+
+    public string value;
+    public Vector2 scroll;
+    public Dictionary<string, string> dictionary;
+
+    private void OnEnable() 
+    {
+      dictionary = LocalisationSystem.GetDictionaryForEditor();  
+    }
+
+    public void OnGUI() 
+    {
+      EditorGUILayout.BeginHorizontal("Box");
+      EditorGUILayout.LabelField("Search: ", EditorStyles.boldLabel);
+      value = EditorGUILayout.TextField(value);
+      EditorGUILayout.EndHorizontal();
+
+      GetSearchResults();
+    }
+
+    private void GetSearchResults() 
+    {
+      if (value == null) {
+        return;
+      }
+
+      EditorGUILayout.BeginVertical();
+      scroll = EditorGUILayout.BeginScrollView(scroll);
+      foreach (KeyValuePair<string, string> element in dictionary) {
+        if (element.Key.ToLower().Contains(value.ToLower()) || element.Value.ToLower().Contains(value.ToLower())) {
+          EditorGUILayout.BeginHorizontal("box");
+          Texture closeIcon = (Texture)Resources.Load("close");
+
+          GUIContent content = new GUIContent(closeIcon);
+
+          if (GUILayout.Button(content, GUILayout.MaxWidth(20), GUILayout.MaxHeight(20))) {
+            if (EditorUtility.DisplayDialog("Remove Key" + element.Key + "?", "This will remove the element from localisation, are you sure?", "Do it.")) {
+              LocalisationSystem.Remove(element.Key);
+              AssetDatabase.Refresh();
+              LocalisationSystem.Init();
+              dictionary = LocalisationSystem.GetDictionaryForEditor();
+            }
+          }
+
+          EditorGUILayout.TextField(element.Key);
+          EditorGUILayout.LabelField(element.Value);
+          EditorGUILayout.EndHorizontal();
+        }
+      }
+
+      EditorGUILayout.EndScrollView();
+      EditorGUILayout.EndVertical();
+    }
+}
